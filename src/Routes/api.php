11766dd95b70b5caae92b763201aa4f972ea54e2
nichozuo <nichozuo@qq.com>
2021-02-04 17:52:38 +0800

    修改api地址

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Nichozuo\LaravelCodegen\Controller\DocsController;
 use Nichozuo\LaravelUtils\Helper\RouteHelper;
 
-Route::prefix('/docs')->name('docs.')->group(function ($router) {
+Route::middleware('api')->prefix('/api/docs')->name('docs.')->group(function ($router) {
     if (config('app.debug')) {
         RouteHelper::New($router, DocsController::class);
     }
